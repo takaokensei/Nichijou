37cd2cb16f885c86f2e31537bfdd04ce7008a18a
takaokensei <68575177+takaokensei@users.noreply.github.com>
2026-05-15 23:32:59 -0300

    style: implement three-font system (Fraunces, Inter, JetBrains Mono)

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,7 @@ module.exports = {
     extend: {
       fontFamily: {
         sans: ['"Inter"', 'sans-serif'],
-        display: ['"Inter"', 'sans-serif'],
+        display: ['"Fraunces"', 'serif'],
         body: ['"Inter"', 'sans-serif'],
         mono: ['"JetBrains Mono"', 'monospace'],
       },
